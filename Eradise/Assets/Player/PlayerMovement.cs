3abb00801aada75f4fb2c952dff2ce3c9e5be032
vi <reducedprocess@gmail.com>
2018-04-20 06:17:17 -0400

    add hole in terrain for elevator, work on movement

--- Eradise/Assets/Player/PlayerMovement.cs
+++ Eradise/Assets/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour {
 	public float runSpeed = 40.0f;
 	private float distanceToGround;
 	private float rotationDegreesPerSecond = 600f;
+	private Animator anim;
 
 	//states
 	public bool running = false;
@@ -21,91 +22,56 @@ public class PlayerMovement : MonoBehaviour {
 	private Transform camera;
 
 	void Start() {
+		anim = GetComponent<Animator>();
 		camera = GameObject.Find("Camera").GetComponent<Transform>();
 		rb = GetComponent<Rigidbody>();
 		distanceToGround = GetComponent<Collider>().bounds.extents.y + 0.001f;
 	}
 
-	//handle running
-	void Update() {
-		if (!stoppedRunning) {
-			if (Input.GetAxis("RightTrigger") > 0.2f || Input.GetKey(KeyCode.LeftShift)) {
-				if (!running) {
-					resetState();
-					running = true;
-				}
-			} else if (running) {
-				resetState();
-				stoppedRunning = true;
-				StartCoroutine("StoppedRunning");
-			}
-		}
-	}
-
 	void FixedUpdate() {
-		//fix to ground
-		// int tries = 0;
-		// while (!CheckIfGrounded() && tries < 10) {
-		// 	tries++;
-		// 	rb.velocity = new Vector3(rb.velocity.x, -100, rb.velocity.z);
-		// 	Debug.Log("hi");
-		// }
 
-		if (!stoppedRunning) {
-			//gets input
-			float horizontal = 0f;
-			float vertical = 0f;
-			if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f) horizontal = Input.GetAxis("Horizontal");
-			if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f) vertical = Input.GetAxis("Vertical");
+		if (CheckIfGrounded()) Debug.Log("grounded");
 
-			//creates a vector direction from the inputs
-			Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+		if (!CheckIfGrounded()) transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
 
-			//if there's an input from the player
-			if (horizontal != 0f || vertical != 0f) {
+		rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
-				if (!running) {
-					resetState();
-					walking = true;
-				}
+		//gets input
+		float horizontal = 0f;
+		float vertical = 0f;
+		if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f) horizontal = Input.GetAxis("Horizontal");
+		if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f) vertical = Input.GetAxis("Vertical");
 
-				Transform newDir = camera;
-				newDir.Rotate(Vector3.left, camera.localRotation.eulerAngles.x);
+		//creates a vector direction from the inputs
+		Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
 
-				direction = newDir.TransformDirection(direction);
-				direction.y = 0.0f;
+		//if there's an input from the player
+		if (horizontal != 0f || vertical != 0f) {
 
-				//normalize input
-				if (direction.magnitude > 1 || direction.magnitude < -1) direction = Vector3.Normalize(direction);
+			Transform newDir = camera;
+			newDir.Rotate(Vector3.left, camera.localRotation.eulerAngles.x);
 
-				//map max speed to input intensity
-				float localMaxSpeed;
+			direction = newDir.TransformDirection(direction);
+			direction.y = 0.0f;
 
-				if (running) localMaxSpeed = Remap(direction.magnitude / 1f, 0f, 1f, 0f, runSpeed);
-				else localMaxSpeed = Remap(direction.magnitude / 1f, 0f, 1f, 0f, maxSpeed);
+			//normalize input
+			if (direction.magnitude > 1 || direction.magnitude < -1) direction = Vector3.Normalize(direction);
 
-				//if (!running && rb.velocity.magnitude > localMaxSpeed) rb.velocity = Vector3.Normalize(rb.velocity) * localMaxSpeed * 2;
+			//rotates the character so it faces the same orientation as the vector direction
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), rotationDegreesPerSecond * Time.deltaTime);
 
-				//rotates the character so it faces the same orientation as the vector direction
-				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), rotationDegreesPerSecond * Time.deltaTime);
+			//move character
+			anim.SetFloat("walk", direction.magnitude);
 
-				//keep Y magnitude so as to not affect jump physics with speed clamp
-				float yMag = rb.velocity.y;
-
-				//moves the character towards vector direction
-				//speed limited by default as a result of normalized movement vector and localMaxSpeed multiplier
-				Vector2 movement = new Vector2(rb.velocity.x, rb.velocity.z);
-				Vector2 newMove = new Vector2(direction.x * localMaxSpeed, direction.z * localMaxSpeed);
-				if (newMove.magnitude > movement.magnitude) {
-					if (CheckIfGrounded()) movement = newMove;
-					else movement = Vector2.Lerp(movement, newMove, 0.07f);
-				}
-
-				rb.velocity = new Vector3(movement.x, yMag, movement.y);
+			//move cloth forward
+			if (direction.magnitude > 0.7f) {
+				GameObject.Find("Cloth").transform.localPosition = new Vector3(0, 0, 0.25f);
+				Debug.Log(GameObject.Find("Cloth").transform.position.z);
 			} else {
-				resetState();
-				idle = true;
+				GameObject.Find("Cloth").transform.localPosition = new Vector3(0, 0, 0.1f);
 			}
+		} else {
+			rb.velocity = new Vector3(0, rb.velocity.y, 0);
 		}
 	}
 
@@ -116,21 +82,6 @@ public class PlayerMovement : MonoBehaviour {
 
 	//check if the player is grounded
 	bool CheckIfGrounded() {
-		return Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
-	}
-
-	void resetState() {
-		idle = false;
-		running = false;
-		stoppedRunning = false;
-		walking = false;
-	}
-
-	//change velocity temporarily for slowdown
-	IEnumerator StoppedRunning() {
-		rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
-		yield return new WaitForSeconds(0.5f);
-		resetState();
-		idle = true;
+		return Physics.Raycast(new Vector3(transform.position.x, transform.position.y - 0.04f, transform.position.z), -Vector3.up, 0.5f);
 	}
 }
